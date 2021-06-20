e4e076a017b6d192c54c5d9f62d854a0980b8f82
tamagram <tamagram64@gmail.com>
2021-06-21 00:00:30 +0900

    :memo: Add script for `ls` command.

---
+++ ls.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <err.h>
+
+// しゃきょー
+
+int main(int argc, char **argv)
+{
+  char *target_path;
+  if (argc > 2)
+    target_path = argv[1];
+  else
+    target_path = ".";
+
+  DIR *target = opendir(target_path);
+  if (target == NULL)
+    err(1, "opendir");
+
+  struct dirent *d;
+
+  while ((d = readdir(target)) != NULL)
+  {
+    printf("%s\n", d->d_name);
+  }
+}
